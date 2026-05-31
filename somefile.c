449541db7d27f282fa7ec9d3146f6392128e76a4
Andrea Landella <andreagiuseppe.landella@gmail.com>
2026-05-31 03:05:30 +0200

    Implement main function with multiple print statements
    Added a main function that prints various strings.

---
+++ somefile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+    printf("blorf zznkqt wibble flargh moop\n");
+    printf("snurgle bweep thnk vrzzzt quonk\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    printf("glibber wobzzt fnerk sploosh\n");
+    return 0;
+}
